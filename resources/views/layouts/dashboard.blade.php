54358efbf972de5a552f5a38c0402441f3285231
Anderson Marin <andersonarturomarin@gmail.com>
2025-05-07 17:27:56 -0500

    Actualizar el título de la página a "Anderson"

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>Dashboard | Finanzas</title>
+    <title>Anderson</title>
     @vite('resources/css/app.css', 'resources/js/app.js')
 </head>
 
